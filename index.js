91cbbb88aec53decd2b9c34c7c5d565d29911912
beellz <belal20795@gmail.com>
2018-09-25 21:53:56 +0530

    added script into differnet file

---
+++ index.js
@@ -0,0 +1,19 @@
+// <script type="text/javascript">
+
+		function generateRandomNumber(){
+			var rand = Math.floor(Math.random() * 10);
+			document.getElementById('display').innerText = rand;
+	
+		} 
+	// </script>
+	
+// <script type="text/javascript">
+
+	function minmax() {
+
+		var rand = Math.random();
+		document.getElementById('min').innerText = rand;
+	}
+
+// </script>
+
